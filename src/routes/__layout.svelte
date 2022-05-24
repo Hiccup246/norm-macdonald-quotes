1d1e6ce435612c6ca8a9f695a0930f89fa800874
James Watt <james.watt529@outlook.com>
2022-05-24 18:54:50 +0100

    Correct layout with position absolute

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,6 +2,7 @@
 	import '../app.css';
 </script>
 
+<div class="app">
 <header>
 	<h1>Norm Macdonald</h1>
 </header>
@@ -14,7 +15,13 @@
 	<p>Made with lots of ❤️ for the man and his legacy</p>
 </footer>
 
+</div>
 <style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
 	main {
 		position: absolute;
 		top: 100px;
@@ -22,22 +29,30 @@
 		flex: 1;
 		display: flex;
 		flex-direction: column;
-		width: 100%;
 		max-width: 1024px;
 		margin: 0 auto;
 		box-sizing: border-box;
+		width: 100%;
+		/* width: calc(100% - 32px); */
+		padding: 0 16px;
 	}
 
 	header {
 		position: absolute;
+		max-width: 1024px;
 		top: 0;
 		width: 100%;
+		/* width: calc(100% - 32px); */
+		padding: 0 16px;
 	}
 
 	footer {
 		position: absolute;
 		bottom: 0;
+		max-width: 1024px;
 		width: 100%;
+		/* width: calc(100% - 32px); */
+		padding: 0 16px;
 	}
 
 	footer p {
