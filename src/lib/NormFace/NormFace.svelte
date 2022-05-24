e5153ef33e79eb1985a44d46b0efb55a47e096ec
James Watt <james.watt529@outlook.com>
2022-05-24 19:51:49 +0100

    Create norm face component

---
+++ src/lib/NormFace/NormFace.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import logo from './norm-face.webp';
+</script>
+
+<div class="norm-face">
+	<div class="norm-face-wrapper">
+		<img src={logo} alt="Norm Face" class="norm-face-image" />
+		<div class="layer1"/>
+		<div class="layer2"/>
+	</div>
+</div>
+
+<style>
+  .norm-face {
+		width: 218px;
+		height: 214px;
+		position: relative;
+	}
+
+	.norm-face-wrapper {
+		width: 200px;
+		height: 200px;
+		position: relative;
+	}
+
+	.norm-face-image {
+		z-index: 5;
+		width: inherit;
+		height: inherit;
+		position: absolute;
+	}
+
+	.layer1 {
+		background-color: #DF80FF;
+		position: absolute;
+		top: 7px;
+		left: 9px;
+		height: inherit;
+		width: inherit;
+		z-index: 4;
+	}
+
+	.layer2 {
+		background-color: #00CDCD;
+		position: absolute;
+		top: 14px;
+		left: 18px;
+		height: inherit;
+		width: inherit;
+		z-index: 3;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,9 @@
 </script>
 
 <script lang="ts">
-
+	import QuoteBox from '$lib/QuoteBox.svelte';
+	import QuoteButton from '$lib/QuoteButton.svelte';
+	import NormFace from '$lib/NormFace/NormFace.svelte';
 </script>
 
 <svelte:head>
@@ -12,9 +14,32 @@
 </svelte:head>
 
 <section>
-  Test
+	<NormFace />
+
+	<div class="quote-box">
+		<p>“I always told everybody the perfect joke would be where the setup and punch line were identical” - Norm Macdonald</p>
+	</div>
+
+	<NormFace />
 </section>
 
 <style>
+	section {
+		width: 100%;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+	}
 
+	.quote-box {
+		background-color: var(--secondary-color);
+		border-radius: 5px;
+		height: 250px;
+		width: 400px;
+		padding: 20px;
+	}
+
+	.quote-box p {
+		margin: 0;
+	}
 </style>
