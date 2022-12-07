79e2ffe5048cd98096c96c72608f89b63f70aa32
James Watt <james.watt529@outlook.com>
2022-12-07 10:53:58 +0000

    Re-organise header tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,25 +5,6 @@
 </script>
 
 <svelte:head>
-	<meta charset="utf-8" />
-	<title>Norm Macdonald Quotes</title>
-	<meta name="description" content="Norm Macdonald Quote Generator" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<meta name="robots" content="index, follow" />
-	<meta
-		name="keywords"
-		content="Norm Macdonald, Norm Macdonald Quotes, Norm Macdonald Quote Generator, Norm Macdonald SNL, Norm Macdonald Standup Comedy, Norm Macdonald Comedy"
-	/>
-	<meta name="author" content="James Watt" />
-	<link rel="canonical" href="https://www.normmacdonaldquotes.com/" />
-	<link rel="manifest" href="/site.webmanifest" />
-	<link href="https://www.normmacdonaldquotes.com/" rel="publisher" />
-
-	<link rel="icon" href="/favicon.ico" />
-	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-
 	<!-- Essential META Tags -->
 	<meta property="og:title" content="Norm Macdonald Quotes" />
 	<meta property="og:type" content="website" />
@@ -37,7 +18,7 @@
 		content="Norm Macdonald Quote Generator"
 	/>
 	<meta property="og:site_name" content="Norm Macdonald Quotes" />
-	<meta name="twitter:image:alt" content={NormFaceImage} />
+	<meta name="twitter:image:alt" content="%sveltekit.assets%/norm-face.webp" />
 
 	<!-- Non-Essential, But Required for Analytics -->
 	<meta name="twitter:site" content="@jameswattnz" />
